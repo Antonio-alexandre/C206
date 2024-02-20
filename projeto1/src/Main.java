19a31532971b8aae0009f3664253c1c1a195ae16
Antonio <antoninho.ab.silva@gmail.com>
2024-02-20 13:46:37 -0300

    Adicionei a pasta referente à primeira aula

---
+++ projeto1/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+     public static void main(String[] args){
+         int x = 10;
+         iny y = 20;
+         System.out.println("Olá Java");
+         System.out.println(x+y);
+         System.out.println("A soma de x e y é: " + (x+y));
+     }
+}
